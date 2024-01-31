8d59e12c133167dfee4f3847bc1bcd38a688d3c3
Frizlab <fload@me.com>
2024-01-31 02:18:09 +0100

    Add ptsname in GNU source exports

--- Sources/CGNUSourceExports/exports.c
+++ Sources/CGNUSourceExports/exports.c
@@ -1,8 +1,14 @@
 #define _GNU_SOURCE
-#include <unistd.h>
+#include <unistd.h> /* execvpe */
+#include <stdlib.h> /* ptsname */
 
 
 
 int spi_execvpe(const char *file, char *const argv[], char *const envp[]) {
 	return execvpe(file, argv, envp);
+}
+
+
+char *spi_ptsname(int fd) {
+	return ptsname(fd);
 }
--- Sources/CGNUSourceExports/exports.h
+++ Sources/CGNUSourceExports/exports.h
@@ -10,4 +10,11 @@
 
 int spi_execvpe(const char *file, char *const argv[], char *const envp[]);
 
+
+/* In the same principle of importing the less stuff possible, we declare ptsname ourselves,
+ *  though in this case we most definitely could import the whole of stdlib.h w/ _GNU_SOURCE set to 500 w/o conflict (we do it in the exports for the tests). */
+
+char *spi_ptsname(int fd);
+
+
 #endif /* exports_h */
