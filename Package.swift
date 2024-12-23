340a394b94ce91fca7323fab6c195639e0f8ad8f
Frizlab <fload@me.com>
2024-12-24 00:15:35 +0100

    Make compilation on Swift 5.5 potentially possible

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let needsGNUSourceExports = true
 #endif
 
 let isXcode = ProcessInfo.processInfo.environment["__CFBundleIdentifier"]?.lowercased().contains("xcode") ?? false
-let executableName = if isXcode {"ProcessInvocationBridge"} else {"swift-process-invocation-bridge"}
+let executableName = isXcode ? "ProcessInvocationBridge" : "swift-process-invocation-bridge"
 
 
 let package = Package(
@@ -37,9 +37,9 @@ let package = Package(
 		res.append(.package(url: "https://github.com/apple/swift-argument-parser.git",         from: "1.2.2"))
 		res.append(.package(url: "https://github.com/apple/swift-log.git",                     from: "1.5.2"))
 		res.append(.package(url: "https://github.com/Frizlab/UnwrapOrThrow.git",               from: "1.0.1"))
-		res.append(.package(url: "https://github.com/xcode-actions/clt-logger.git",            from: "0.5.1"))
-		res.append(.package(url: "https://github.com/xcode-actions/stream-reader.git",         from: "3.5.0"))
-		res.append(.package(url: "https://github.com/xcode-actions/swift-signal-handling.git", from: "1.1.1"))
+		res.append(.package(url: "https://github.com/xcode-actions/clt-logger.git",            from: "1.0.0-beta.4"))
+		res.append(.package(url: "https://github.com/xcode-actions/stream-reader.git",         from: "3.6.0"))
+		res.append(.package(url: "https://github.com/xcode-actions/swift-signal-handling.git", .upToNextMinor(from: "1.1.2")))
 #if !canImport(System)
 		res.append(.package(url: "https://github.com/apple/swift-system.git",                  from: "1.0.0"))
 #endif
